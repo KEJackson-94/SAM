2b5e5e589cdcc8d08d980b6c7820435282619867
Kevin Jackson <kevin.jackson@umces.edu>
2023-03-30 00:38:45 -0400

    Started script templates

---
+++ scripts/data_raw.R
@@ -0,0 +1,51 @@
+
+
+###########################
+#### Social INDICATORS ####
+###########################
+
+#### Crop Diversity (H Index)
+
+#### Food Affordability (RSE)
+
+#### Under-nourishment (UDN)
+
+#### Gender Gap Score (GGG)
+
+#### Rural Poverty (RPV)
+
+#### Land Rights (LRS)
+
+
+#############################
+#### Economic INDICATORS ####
+#############################
+
+#### Labor Productivity (AGDP)
+
+#### Finance Access (A2F)
+
+#### Price Volatility (PVOL)
+
+#### Government Support (AEXP)
+
+#### Trade Openness (TROP)
+
+#### Food Loss (FLP)
+
+##################################
+#### Environmental INDICATORS ####
+##################################
+
+#### Water Consumption (SUSI)
+
+#### N Surplus (Nsur)
+
+#### P Surplus (Psur)
+
+#### Land Use Change (LUC)
+
+#### Greenhouse Gas Emmissions (GHG)
+
+#### Soil Erosion (SER)
+
